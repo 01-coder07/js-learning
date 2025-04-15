9663581f9af1324a44a55f1ee5896d72e1d18ab5
01-coder07 <154795656+01-coder07@users.noreply.github.com>
2025-04-15 19:22:22 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 124223
+let accountEmail = "harshit@yahho.com"
+var accountPassword = "123121"
+accountCity = "haldwani"
+
+//accountId=388
+//console.log(accountId);
+
+accountEmail = "pant2005@gmail.com"
+accountPassword = "12322222"
+accountCity="Nainital"
+let myname;
+/*
+  dont use var because of global scope . so use mostly const or let
+*/
+console.table([accountEmail,accountId,accountPassword,accountCity,myname]);
+
