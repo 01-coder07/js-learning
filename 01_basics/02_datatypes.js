0f86a4bd4440e30aed5ab17e33efb93828c51800
01-coder07 <154795656+01-coder07@users.noreply.github.com>
2025-04-16 17:00:34 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3)  we are using node js ,not browser
+console.log(
+    3+3                // this is not good . Code reusability should be high
+)
+console.log("ELephant")
+
+
+//datatypes
+let a= 30
+let name ="Harshit"
+let loggedIn = true;
+
+//primitive DATAtype
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false;
+//null => standAlone Value
+//undefined => you didnt defined in a variable let a
+//symbol => unique
+
+//non primitve 
+//objects
+//arrays
+console.log(typeof 12);
+console.log(typeof "Harshit");
+console.log(typeof null);          //typeof null is OBJECT.
+console.log(typeof true);            
+console.log(typeof undefined);     //typeof undefined is undefined.
+
+
+
+
+
+
+
