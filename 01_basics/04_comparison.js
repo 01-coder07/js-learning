e910c5274aca891204bcf31bbf973cadf8fe35e9
01-coder07 <154795656+01-coder07@users.noreply.github.com>
2025-04-17 07:14:34 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(3>2);
+// console.log(3>=2);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">=2);
+console.log(null > 0);   
+console.log(null === 0);    //Note: == and >= or <= work differntly.
+console.log(null>=0);
+console.log(null<=0);
+console.log("harshitpant"> "harshit");
+console.log(false > 4);
+
+
+
+
+
+
+
