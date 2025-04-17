fb2d020498ff6a1867e8add2400fd2b29306ca15
01-coder07 <154795656+01-coder07@users.noreply.github.com>
2025-04-17 06:19:11 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = "jss";
+//console.log(typeof score);
+///console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+// console.log(valueInNumber);  NaN
+//console.log(valueInNumber);
+
+/*
+ "33"=> 33
+  "33abcd"=>NaN
+  true=>1;false =>0  
+*/
+let loggedIn = "";
+let boolInLogging = Boolean(loggedIn)
+//console.log(boolInLogging);
+
+/* 
+   boolean:
+   0=>false;   1=>true
+   ""=>false    "Harshit"=>true
+*/
+let someNumber = 33
+let someString = String(someNumber)
+console.log(someString);
+console.log(typeof someString);
+
+
