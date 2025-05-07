cc0ca2872d9578e560dab9738b54212a484bcedb
01-coder07 <154795656+01-coder07@users.noreply.github.com>
2025-05-07 11:10:43 +0000

    nums

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,16 @@
+const num = 34;
+console.log(num);
+
+const balance = new Number(344);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 322.12;
+// console.log(otherNumber.toPrecision(4));
+
+const hundread = 1000000;
+// console.log(hundread.toLocaleString('en-In'));
+
+
+// +++++++++++ Maths ++++++++++++++
+
