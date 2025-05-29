1427324727bc3de4520fffb37475a67c09ae0b6d
01-coder07 <154795656+01-coder07@users.noreply.github.com>
2025-05-29 17:59:53 +0000

    date.js

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,15 @@
+//date
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+
+let mycreatedDate = new Date("05-29-2025");
+console.log(mycreatedDate.toLocaleString());
+
+
+
+
+
