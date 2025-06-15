d24114d3be6a7b2283f028f8c6a76a802b166723
01-coder07 <154795656+01-coder07@users.noreply.github.com>
2025-06-15 16:56:56 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,30 @@
+// let a =20;
+// const b =10;
+// var c =30;
+if(true){
+    let a =20;
+    const b =10;
+    var c =30;
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(f);
+// var f;
+// console.log(h);
+// let h;
+
+function one (){
+    const username ='Harshit'
+
+    function two(){
+        const webSite = "Youtube"
+        console.log(username);
+    }
+    two();
+    // console.log(webSite);
+}
+one();
+
+
+
