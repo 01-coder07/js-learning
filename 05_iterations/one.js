e1b77cf6d95533dd910bf90947e8681f65afd051
01-coder07 <154795656+01-coder07@users.noreply.github.com>
2025-06-18 17:29:13 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,10 @@
+for(let a = 1;a<=10;a++){
+    let user =a;
+    // console.log(user);
+}
+ // table for 6
+ for(let i =6;i<=6;i++){
+     for(let j=1;j<=10;j++){
+        // console.log(`${i} * ${j}= ${i*j}`);
+     }
+ }
