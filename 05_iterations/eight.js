b0b3bc20b0180d690f98a6c7d9307d71c553c9b5
01-coder07 <154795656+01-coder07@users.noreply.github.com>
2025-06-20 17:12:58 +0000

    reduce

---
+++ 05_iterations/eight.js
@@ -0,0 +1,30 @@
+const myNums =[1,2,3,4,5];
+
+// const total = myNums.reduce( function (acc,currval){
+//     console.log(`acc :${acc} and currval :${currval}`);  
+//     return acc+currval;
+// },0);
+// console.log(total);
+//Arrow function:
+
+const total = myNums.reduce((acc,currval)=>acc+currval,0)
+console.log(total);
+
+
+const shoppingCart = [
+    {
+          itemName:"Js course",
+          price:1999,
+    },
+    {
+      itemName:"py course",
+      price:1500,
+    },
+    {
+      itemName:"datascience",
+      price:12999,
+    }
+]
+const totalPrice = shoppingCart.reduce((acc,item)=>acc+item.price,0);
+console.log(totalPrice);
+
