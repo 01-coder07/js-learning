da87410df563caa6d1b63e1520a48c3da194a302
01-coder07 <154795656+01-coder07@users.noreply.github.com>
2025-06-20 17:13:17 +0000

    maps

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,4 +1,10 @@
 const myNums = [1,2,3,4,5,6,7,8,9,10];
-const newN = myNums.map((num)=>num+10)
+// const newN = myNums.map((num)=>num+10)
+
+// we can use .map().map() as well;
+const newN = myNums.map((num)=>{ 
+    return  num +10})
+    .map((num)=>{return num*2})
+    .filter((num)=> num>30 && num<35)
 console.log(newN);
 
